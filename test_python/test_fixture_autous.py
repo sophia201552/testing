f33245411c907e493d99a4ed4f0fc5adf8c3636e
sophia201552 <1281056983@qq.com>
2018-01-10 16:24:09 +0800

    pytest,fixture的相关功能

---
+++ test_python/test_fixture_autous.py
@@ -0,0 +1,21 @@
+import time
+import pytest
+
+@pytest.fixture(scope="module", autouse=True)
+def mod_header(request):
+    print('\n-----------------')
+    print('module      : %s' % request.module.__name__)
+    print('-----------------')
+
+@pytest.fixture(scope="function", autouse=True)
+def func_header(request):
+    print('\n-----------------')
+    print('function    : %s' % request.function.__name__)
+    print('time        : %s' % time.asctime())
+    print('-----------------')
+
+def test_one():
+    print('in test_one()')
+
+def test_two():
+    print('in test_two()')
